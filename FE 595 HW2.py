a0d3b3c7740b3fa6c43c90cb2b84b0159bf54e52
TianyiY519 <70730404+TianyiY519@users.noreply.github.com>
2020-10-03 19:55:32 -0400

    Add files via upload

---
+++ FE 595 HW2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 16:47:23 2020
+
+@author: tianyiyang
+"""
+
+
+import requests 
+from bs4 import BeautifulSoup 
+
+with open("inputs.txt",'w') as f:
+    for i in range(0,50):
+        res = requests.get('http://3.95.249.159:8000/random_company')
+        html = res.text
+        soup = BeautifulSoup( html,'html.parser') 
+        items = soup.find_all( 'li' )
+        name = items[0]
+        f.write(name.text)
+        f.write('\n')
+        for item in range(0,len(items)):
+            if items[item].text[0]=='P':
+                purpose = items[item]
+            
+                f.write(purpose.text)
+                f.write('\n')
+    f.close()
+#name.text,purpose.text
