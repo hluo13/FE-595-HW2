a416f8d1abfba86e39182968ab7991778bb8f1da
TianyiY519 <70730404+TianyiY519@users.noreply.github.com>
2020-10-04 00:50:10 -0400

    Update FE 595 HW2.py

--- FE 595 HW2.py
+++ FE 595 HW2.py
@@ -9,20 +9,22 @@ Created on Sat Oct  3 16:47:23 2020
 
 import requests 
 from bs4 import BeautifulSoup 
-
-with open("inputs.txt",'w') as f:
-    for i in range(0,50):
-        res = requests.get('http://3.95.249.159:8000/random_company')
-        html = res.text
-        soup = BeautifulSoup( html,'html.parser') 
-        items = soup.find_all( 'li' )
-        name = items[0]
-        f.write(name.text)
-        f.write('\n')
-        for item in range(0,len(items)):
-            if items[item].text[0]=='P':
-                purpose = items[item]
+def hw2():
+    with open("inputs.txt",'w') as f:
+        for i in range(0,50):
+            res = requests.get('http://3.95.249.159:8000/random_company')
+            html = res.text
+            soup = BeautifulSoup( html,'html.parser') 
+            items = soup.find_all( 'li' )
+            name = items[0]
+            f.write(name.text)
+            f.write('\n')
+            for item in range(0,len(items)):
+                if items[item].text[0]=='P':
+                    purpose = items[item]
             
-                f.write(purpose.text)
-                f.write('\n')
-    f.close()
+                    f.write(purpose.text)
+                    f.write('\n')
+        f.close()
+if __name__ == "__main__":
+    hw2()
